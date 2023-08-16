4bc5007bc8ffa952690c23ef486dbc676b4cb2bf
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-16 16:36:34 +0545

    changes

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,8 @@ import React, {Component} from 'react';
 class Button extends Component {
     render(){
         return(
-            <div>
-                <button class="calc-button"></button>
+            <div >
+                <button class="calc-button" onClick={() => this.props.action(this.props.symbol)}/>
             </div>
         )
     }
